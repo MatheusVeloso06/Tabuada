bf453a85d1afe9941db2279025ae5af5431cd417
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-10-31 20:43:27 -0300

    Atividade Realizada

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("--- Tabuada ---");
+
+int n = 1;
+int x; // Altere para 
+
+x = Convert.ToInt32(Console.ReadLine());
+n = Convert.ToInt32(Console.ReadLine());
+
+while (n <= 10)
+{
+    Console.WriteLine($"{x} x {n} = {x * n}");
+
+    n = n + 1;
+}
